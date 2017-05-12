0c9fd888224f989ab98be443c61e0fd09659f899
ellisthomas <ellisthomas80@gmail.com>
2017-05-11 23:02:50 -0500

    mvp

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,21 +2,41 @@ var app = angular.module("ZoeApp", []);
 
 
 app.controller("ItemCtrl", ($scope) => {
-	$scope.food = "Yummy!";
-	$scope.showListView = true;
-	$scope.items = [
+	$scope.showListView =false;
+		$scope.items = [
         {
-          id: 0,
-          food: "donuts"
+          favFood: false,
+          food: "Tacos"
+          // image: "http://i.amz.mshcdn.com/Q0DWc2Odlo3P-ZrsNoJ37sIvaCk=/1200x627/2015%2F05%2F26%2F95%2FPizzaHutmea.d0b39.jpg"
         },
         {
-          id: 1,
-          food: "pizza"
+          favFood: false,
+          food: "Pizza"
         },
         {
-          id: 2,
-          food: "skittles"
-        }
+          favFood: true,
+          food: "Donuts"
+        },
+        {
+          favFood: true,
+          food: "Cookies"
+        },
+        {
+          favFood: true,
+          food: "Pie"
+        },
+        {
+          favFood: true,
+          food: "Cherry Pie"
+        },
+        {
+          favFood: true,
+          food: "Apple Pie"
+        },
+        {
+          favFood: true,
+          food: "Did we mention Pie"
+        },
       ];
 
 $scope.newItem = () => {
@@ -24,8 +44,8 @@ $scope.newItem = () => {
 	};	
 
 
-$scope.allItems = () => {
-		$scope.showListView = true;
+$scope.zoeFood = () => {
+		$scope.showListView = !$scope.showListView;
 	};
 
 });
