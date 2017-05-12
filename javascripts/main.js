f05ad47e313513b1937ff3cb7c4301f38cd23e62
ellisthomas <ellisthomas80@gmail.com>
2017-05-11 20:03:55 -0500

    printing to page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,31 @@
 var app = angular.module("ZoeApp", []);
+
+
+app.controller("ItemCtrl", ($scope) => {
+	$scope.food = "Yummy!";
+	$scope.showListView = true;
+	$scope.items = [
+        {
+          id: 0,
+          food: "donuts"
+        },
+        {
+          id: 1,
+          food: "pizza"
+        },
+        {
+          id: 2,
+          food: "skittles"
+        }
+      ];
+
+$scope.newItem = () => {
+		$scope.showListView = false;
+	};	
+
+
+$scope.allItems = () => {
+		$scope.showListView = true;
+	};
+
+});
